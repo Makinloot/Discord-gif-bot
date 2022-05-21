939079ea1eb4bb8b12229acf86b86563c420bcbd
Tornike Epitashvili <itorn9777@gmail.com>
2022-05-21 15:11:43 -0700

    added error handling

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ client.on('messageCreate', sendGif); // calls for sendGif function on message
 
 async function sendGif(msg) {
     const searchTerm = msg.content.split(' ');
-    if(searchTerm[0] === '!gif') {
-        let keyword;
-        if(searchTerm.length > 1) {
-            keyword = searchTerm.slice(1, searchTerm.length).join(' ');
-        }
-        const gifUrl = `https://g.tenor.com/v1/search?q=${keyword}&key=${process.env.APIKEY}&contentfilter=off`;
-        
-        const response = await fetch(gifUrl);
-        const data = await response.json();
-        const index = Math.floor(Math.random() * data.results.length)
-        msg.channel.send(data.results[index].url);
+    try {
+        if(searchTerm[0] === '!gif') {
+            let keyword;
+            if(searchTerm.length > 1) {
+                keyword = searchTerm.slice(1, searchTerm.length).join(' ');
+            }
+            const gifUrl = `https://g.tenor.com/v1/search?q=${keyword}&key=${process.env.APIKEY}&contentfilter=off`;
+            
+            const response = await fetch(gifUrl);
+            const data = await response.json();
+            const index = Math.floor(Math.random() * data.results.length)
+            msg.channel.send(data.results[index].url);
+        }        
+    } catch (error) {
+        msg.channel.send('Gif not found :woozy_face:');
     }
+
 }
